22f76c69bb1df9dbdba58fdb077f01a718faad97
leitzkeric <ricardo.leitzke@gmail.com>
2026-05-04 12:16:51 -0300

    estrutura para o agendamento

---
+++ src/main/java/medico/api/domain/consulta/DadosDetalhamentoConsulta.java
@@ -0,0 +1,10 @@
+package medico.api.domain.consulta;
+
+import java.time.LocalDateTime;
+
+public record DadosDetalhamentoConsulta(
+        Long id,
+        Long idMedico,
+        Long idPaciente,
+        LocalDateTime data) {
+}
