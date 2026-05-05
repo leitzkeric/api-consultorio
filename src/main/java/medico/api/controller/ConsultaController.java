db4bb9ddc274231f00145cca02b0ffc238b9c3f5
leitzkeric <ricardo.leitzke@gmail.com>
2026-05-05 18:14:21 -0300

    Inclusao da documentacao Swagger

--- src/main/java/medico/api/controller/ConsultaController.java
+++ src/main/java/medico/api/controller/ConsultaController.java
@@ -1,5 +1,6 @@
 package medico.api.controller;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import medico.api.domain.ValidacaoException;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("consultas")
+@SecurityRequirement(name = "bearer-key")
 public class ConsultaController {
 
     @Autowired
--- src/main/java/medico/api/controller/MedicoController.java
+++ src/main/java/medico/api/controller/MedicoController.java
@@ -1,5 +1,6 @@
 package medico.api.controller;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import medico.api.domain.medico.*;
@@ -12,6 +13,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/medicos")
+@SecurityRequirement(name = "bearer-key")
 public class MedicoController {
 
     @Autowired
